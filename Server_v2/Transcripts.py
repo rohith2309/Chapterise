eb3661a085dc3ad15ddc1b7399d3ca3ec90a4b0d
Rohith <Rohith>
2025-06-24 22:34:51 +0530

    Server V2 Proxy added

--- Server_v2/Transcripts.py
+++ Server_v2/Transcripts.py
@@ -1,13 +1,24 @@
 import json
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.proxies import WebshareProxyConfig
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+PROXY_ID = os.environ.get("PROXY_ID")
+PROXY_PWD = os.environ.get("PROXY_PWD")
 
 def fetch_youtube_transcript(videoId: str) -> str:
     try:
         '''url=f'https://www.youtube.com/watch?v={videoId}'
         loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
         documents = loader.load()'''
-        Ytapi=YouTubeTranscriptApi()
+        Ytapi=YouTubeTranscriptApi(
+            proxy_config=WebshareProxyConfig(
+        proxy_username=PROXY_ID,
+        proxy_password=PROXY_PWD,
+        )
+        )
         documents= Ytapi.get_transcript(videoId, languages=['en'])  
     
         if documents:
